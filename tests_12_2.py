2f6868a42b4bea0bf703322d89050093ddf121b4
Ekaterina-spec-6553 <ekaterinarunova6553@gmail.com>
2024-11-19 01:18:00 +0300

    Create tests_12_2.py

---
+++ tests_12_2.py
@@ -0,0 +1,34 @@
+from runner_and_tournament import Runner, Tournament
+import unittest
+
+class TournamentTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.all_results = {}
+
+    def setUp(self):
+        self.runner1 = Runner("Усэйн", 10)
+        self.runner2 = Runner("Андрей",9)
+        self.runner3 = Runner("Ник",3)
+
+    def tearDownClass(cls):
+        pass
+
+    def  test_beg1(self):
+         tournament = Tournament(90,self.runner1,self.runner2)
+         finishers = tournament.start()
+         TournamentTest.all_results[1] = finishers
+         self.assertTrue(finishers[2].name == "Ник")
+         print(100)
+
+    def  test_beg2(self):
+         tournament = Tournament(90, self.runner2, self.runner3)
+         finishers = tournament.start()
+         TournamentTest.all_results[2] = finishers
+         self.assertTrue(finishers[2].name == "Ник")
+
+    def test_beg3(self):
+        tournament = Tournament(90, self.runner1, self.runner2,self.runner3)
+        finishers = tournament.start()
+        TournamentTest.all_results[3] = finishers
+        self.assertTrue(finishers[3].name=="Ник")
